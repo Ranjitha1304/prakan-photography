57b6f4fe67eed8240600abd23fb4dddda0d113ac
Ranjitha1304 <ranjitha13cs@gmail.com>
2025-05-06 20:37:41 +0530

    Update client.js

--- assets/js/client.js
+++ assets/js/client.js
@@ -1,47 +1,47 @@
 const testimonials = [
     {
       name: 'Anjali & Karthik',
-      img: './assets/images/client1.png',
+      img: 'assets/images/client1.png',
       msg: 'Absolutely loved our photos! The team at Prakan Photo Studio captured our special day perfectly. '
     },
     {
       name: 'Suresh M.',
-      img: '../images/client2.png',
+      img: 'assets/images/client2.png',
       msg: 'Professional, friendly, and creative! I Had a Portrait Session And The Results Were Beyond Expectations. '
     },
     {
       name: 'Divya R.',
-      img: '/assets/images/client3.png',
+      img: 'assets/images/client3.png',
       msg: 'The best photo studio experience ever! From The First Consultation To The Final Album Delivery. .'
     },
     {
       name: 'Meera & Arjun',
-      img: '/assets/images/client3.png',
+      img: 'assets/images/client3.png',
       msg: 'Prakan team truly made our day special with stunning photos and amazing support!'
     },
     {
       name: 'Ravi K.',
-      img: '/assets/images/client2.png',
+      img: 'assets/images/client2.png',
       msg: 'Best decision to hire them! Their eye for detail is unmatched. Highly Recommended!'
     },
     {
       name: 'Sneha L.',
-      img: '/assets/images/client3.png',
+      img: 'assets/images/client3.png',
       msg: 'We were blown away by the pictures. Every emotion captured beautifully!  Everything was smooth'
     },
     {
       name: 'Nithya G.',
-      img: '/assets/images/client1.png',
+      img: 'assets/images/client1.png',
       msg: 'A talented and passionate team. Great post-production work too! Their Attention To Detail And Passion For Photography Really Shows'
     },
     {
       name: 'Deekshitha & Sekar',
-      img: '/assets/images/client2.png',
+      img: 'assets/images/client2.png',
       msg: 'Professional, friendly, and creative! I Had a Portrait Session And The Results Were Beyond Expectations. '
     },
     {
       name: 'Reena Joseph',
-      img: '/assets/images/client3.png',
+      img: 'assets/images/client3.png',
       msg: 'The best photo studio experience ever! From The First Consultation To The Final Album Delivery.'
     }
   ];
